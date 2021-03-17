c7b95e991353f608c38d458cf6f446ce42f35e17
ryabykhms <ryabykhms@gmail.com>
2021-03-17 15:34:23 +0300

    feat: create store

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { defaultState } from './defaultState';
+import { rootReducer } from './rootReducer';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const AppStore = createStore(rootReducer, defaultState, composeEnhancers());
+
+export * from './defaultState';
+export * from './reducers';
+export * from './actions';
