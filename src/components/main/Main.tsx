49b83e9ca29101ca0c6d519b47d479319dad31ac
ryabykhms <ryabykhms@gmail.com>
2021-03-18 12:28:20 +0300

    feat: add h1 and main component styles

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,6 +4,7 @@ import Actions from '../actions/Actions';
 import AdditionInfo from '../additionInfo/AdditionInfo';
 import BaseInfo from '../baseInfo/BaseInfo';
 import Skills from '../skills/Skills';
+import './Main.css';
 
 const Main = () => {
   const baseParams = useSelector((state: IAppState) => state.baseParams);
@@ -13,10 +14,15 @@ const Main = () => {
 
   return (
     <main>
-      <BaseInfo power={power} agility={agility} intellect={intellect} charisma={charisma} />
-      <AdditionInfo lifePower={lifePower} evasion={evasion} energy={energy} />
-      <Skills power={power} agility={agility} intellect={intellect} charisma={charisma} />
-      <Actions />
+      <div className="wrapper">
+        <h1>RPG Character Editor</h1>
+        <BaseInfo power={power} agility={agility} intellect={intellect} charisma={charisma} />
+        <AdditionInfo lifePower={lifePower} evasion={evasion} energy={energy} />
+        <div className="info-group">
+          <Skills power={power} agility={agility} intellect={intellect} charisma={charisma} />
+          <Actions />
+        </div>
+      </div>
     </main>
   );
 };
