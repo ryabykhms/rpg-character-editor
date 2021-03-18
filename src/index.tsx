d73b2fa2d7e88ce5502eb1cf012a6192cd5b1f6a
ryabykhms <ryabykhms@gmail.com>
2021-03-18 09:20:22 +0300

    fix: change app dir name

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppStore } from './store';
 import './index.css';
-import App from './containers/app/App';
+import App from './containers/_app/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
