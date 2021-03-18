031357fe5a8a723579a56cef63aca1b726ff6f15
ryabykhms <ryabykhms@gmail.com>
2021-03-18 12:34:02 +0300

    feat: add addition info component styles, progress, images

--- src/components/additionInfo/AdditionInfo.tsx
+++ src/components/additionInfo/AdditionInfo.tsx
@@ -1,20 +1,28 @@
+import { MAX_ENERGY, MAX_EVASION, MAX_LIFE_POWER } from '../../constants';
 import { IAdditionParamsState } from '../../types';
+import heart from '../../images/heart.svg';
+import escape from '../../images/escape.svg';
+import energy from '../../images/energy.svg';
+import './AdditionInfo.css';
 
 const AdditionInfo = (props: IAdditionParamsState) => {
   return (
-    <div>
-      <hr />
-      <div>
-        <span>Life Power:</span>
-        <span>{props.lifePower}</span>
+    <div className="addition-info">
+      <div className="addition-info__item" title="Life Power">
+        <img className="addition-info__img" src={heart} alt="Life Power" />
+        <progress
+          className="progress progress--life"
+          max={MAX_LIFE_POWER}
+          value={props.lifePower}
+        />
       </div>
-      <div>
-        <span>Evasion:</span>
-        <span>{props.evasion}</span>
+      <div className="addition-info__item" title="Evasion">
+        <img className="addition-info__img" src={escape} alt="Evasion" />
+        <progress className="progress progress--evasion" max={MAX_EVASION} value={props.evasion} />
       </div>
-      <div>
-        <span>Energy:</span>
-        <span>{props.energy}</span>
+      <div className="addition-info__item" title="Energy">
+        <img className="addition-info__img" src={energy} alt="Energy" />
+        <progress className="progress progress--energy" max={MAX_ENERGY} value={props.energy} />
       </div>
     </div>
   );
